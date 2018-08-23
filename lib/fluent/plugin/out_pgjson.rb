0ddfd80c81d68b963a02175cfdb063c89648d0c4
Kenji Okimoto <okimoto@clear-code.com>
2018-08-23 14:20:07 +0900

    Re-define sslmode as a enum value
    Signed-off-by: Kenji Okimoto <okimoto@clear-code.com>

--- lib/fluent/plugin/out_pgjson.rb
+++ lib/fluent/plugin/out_pgjson.rb
@@ -15,7 +15,8 @@ module Fluent::Plugin
     config_param :host, :string, default: "localhost"
     desc "The port of PostgreSQL server"
     config_param :port, :integer, default: 5432
-    config_param :sslmode, :string, default: "prefer"
+    desc "Set the sslmode to enable Eavesdropping protection/MITM protection"
+    config_param :sslmode, :enum, list: %i[disable allow prefer require verify-ca verify-full], default: :prefer
     desc "The database name to connect"
     config_param :database, :string
     desc "The table name to insert records"
